08bc6dcd05396ef9eefa544a809e69f8c3acc461
Dmitry <dima@mail.com>
2021-03-11 09:30:17 +0400

    update prisma connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-import Adapters from 'next-auth/adapters';
-
-const prismaClientPropertyName = `__prevent-name-collision__prisma`;
-type GlobalThisWithPrismaClient = typeof globalThis & {
-  [prismaClientPropertyName]: PrismaClient;
-};
-
-const getPrismaClient = () => {
-  if (process.env.NODE_ENV === `production`) {
-    return new PrismaClient();
-  } else {
-    const newGlobalThis = globalThis as GlobalThisWithPrismaClient;
-    if (!newGlobalThis[prismaClientPropertyName]) {
-      newGlobalThis[prismaClientPropertyName] = new PrismaClient();
-    }
-    return newGlobalThis[prismaClientPropertyName];
-  }
-};
-const prisma = getPrismaClient();
-
-export default Adapters.Prisma.Adapter({ prisma });
+const prisma: PrismaClient = new PrismaClient();
