5cb18aabeaa98793f3c869e80ffd96af223d42be
Dmitry <dima@mail.com>
2021-03-11 10:39:11 +0400

    delete global.prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma: PrismaClient = new PrismaClient();
+const prisma = new PrismaClient();
 
-
-export default prisma
+export default prisma;
